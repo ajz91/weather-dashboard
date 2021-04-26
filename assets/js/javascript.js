bd7d69374ba92bc71ae3f675d14657adfe06c8bd
AJ Ziarek <deltrigger@pm.me>
2021-04-26 18:00:30 -0500

    swapping to city name

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,28 +1,28 @@
 //variable to select the form itself
 var cityFormEl = document.querySelector("#city-form");
-
 //variable to select the city input
-var cityInputEl = document.querySelector("#city");
-
-//fetch API
-var getWeather = function(cityID) {
-    var myKey = '953df2735637440fe1a5a9f207d9928e';
-    fetch('https://api.openweathermap.org/data/2.5/weather?id='+ cityID + '&appid=' + myKey)
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
+var cityInputEl = document.querySelector("#cityInput");
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
-  var cityValue = cityFormEl.value.trim();
+  console.log(event);
 
-  if (cityValue) {
-    getWeather(cityValue);
-    cityInputEl.value= "";
+  var userCityInput = cityInputEl.value.trim();
+  if (userCityInput) {
+    getWeather(userCityInput);
+    userCityInput.value = "";
   } else {
-    alert("Please enter a valid City ID");
+    alert  ("Please enter a valid City");
   }
 };
+
+//fetch API
+var getWeather = function(cityName) {
+    var myKey = '953df2735637440fe1a5a9f207d9928e';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id='+ cityName + '&appid=' + myKey)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
   
   
-cityFormEl.addEventListener("submit", cityFormEl);
+cityFormEl.addEventListener("submit", formSubmitHandler);
